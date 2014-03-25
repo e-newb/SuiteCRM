81cb84e017114744a92fe4f8232b4f3bd9140b13
e-newb <tom@e-newb.net>
2014-03-25 11:45:46 +0100

    Create pl_pl.lang.php

---
+++ modules/ACL/language/pl_pl.lang.php
@@ -0,0 +1,23 @@
+<?php
+
+array(
+'LBL_ALLOW_ALL'=>'Wszystkie',
+'LBL_ALLOW_NONE'=>'Żadne',
+'LBL_ALLOW_OWNER'=>'Właściciel',
+'LBL_ROLE'=>'Rola',
+'LBL_NAME'=>'Imię',
+'LBL_DESCRIPTION'=>'Opis',
+'LIST_ROLES'=>'Listuj Role',
+'LBL_USERS_SUBPANEL_TITLE'=>'Urzytkownicy',
+'LIST_ROLES_BY_USER'=>'Listur Role po Urzytkownikach',
+'LBL_ROLES_SUBPANEL_TITLE'=>'Role Urzytkowników',
+'LBL_SEARCH_FORM_TITLE'=>'Szukaj',
+'LBL_NO_ACCESS'=>'Nie masz dostępu do tego miejsca. Skontaktuj się z Administratorem.',
+'LBL_REDIRECT_TO_HOME'=>'Przeniesienie do Home w',
+'LBL_SECONDS'=>'sekund',
+'LBL_ADDING'=>'Dodawanie dla '
+);
+
+
+
+?>
